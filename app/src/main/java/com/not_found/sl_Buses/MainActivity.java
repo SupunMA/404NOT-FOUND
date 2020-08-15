79d3f3345442dcb093f1ca80428ced542afb9d3a
Supun <supunmadhusanka62@gmail.com>
2020-08-15 19:26:55 +0530

    First Update

---
+++ app/src/main/java/com/not_found/sl_Buses/MainActivity.java
@@ -0,0 +1,14 @@
+package com.not_found.sl_Buses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
